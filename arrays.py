f0b60961a389c1abf6a5817de4f55215de72fb70
Alvin <wangalvin23@gmail.com>
2021-10-01 23:38:12 -0700

    first commit

---
+++ arrays.py
@@ -0,0 +1,8 @@
+length = input("How long do you want your array to be? ")
+print(length)
+for i in range(8):
+    for l in range(int(length)):
+        for j in range(6):
+            print(i, end=" ")
+        print("", end=" ")
+    print()
